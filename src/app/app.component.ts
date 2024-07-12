295349a5134bcf4ca731bb507e27f318e56d1507
spencer <sbirch3303@gmail.com>
2024-07-12 00:06:52 -0600

    started adding CSS on the images and figuring it out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mbirchjewlery_website';
+
+  // s3://mbirchjewelry
+  // http://mbirchjewelry.s3-website.us-east-2.amazonaws.com/
+
+  // http://aggravation-game.s3-website.us-east-2.amazonaws.com/
 }
